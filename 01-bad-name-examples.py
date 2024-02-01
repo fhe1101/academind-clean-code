d535add2957f98c0882cac4949d6bbc7c2ddbfb5
Fusen He <efh003@gmail.com>
2024-02-01 14:29:49 -0500

    Add comments to describe the problem

--- 01-bad-name-examples.py
+++ 01-bad-name-examples.py
@@ -1,22 +1,28 @@
 from datetime import datetime
 
-
+# self is the this keyword in other languages such as C++
 class Entity:
     def __init__(self, title, description, ymdhm):
         self.title = title
         self.description = description
         self.ymdhm = ymdhm
+# Entity seems to be some kind of blog post. ymdhm is a date
 
-
+# Tis is a function, not a method based on the indentation
+# it prints blog post
+## item name is better for blog_post
 def output(item):
     print('Title: ' + item.title)
     print('Description: ' + item.description)
     print('Published: ' + item.ymdhm)
 
 
+# summary is actually  title
 summary = 'Clean Code Is Great!'
 desc = 'Actually, writing Clean Code can be pretty fun. You\'ll see!'
+# new_data is ok, but today or now is better
 new_date = datetime.now()
+# publish like a command, formatted_data is a better name
 publish = new_date.strftime('%Y-%m-%d %H:%M')
 
 item = Entity(summary, desc, publish)
--- 01-challenge-problem.py
+++ 01-challenge-problem.py
@@ -1,18 +1,22 @@
 class Point:
+    # coordX -> x, coordY -> y
     def __init__(self, coordX, coordY):
         self.coordX = coordX
         self.coordY = coordY
 
 
 class Rectangle:
+    # starting_point -> origin, broad -> width, high -> height
     def __init__(self, starting_point, broad, high):
         self.starting_point = starting_point
         self.broad = broad
         self.high = high
 
+    # area -> get_area
     def area(self):
         return self.broad * self.high
 
+    # end_points -> print_coordinates
     def end_points(self):
         top_right = self.starting_point.coordX + self.broad
         bottom_left = self.starting_point.coordY + self.high
@@ -21,14 +25,17 @@ class Rectangle:
         print('End Point X-Axis (Top Right): ' + str(top_right))
         print('End Point Y-Axis (Bottom Left): ' + str(bottom_left))
 
-
+# build_stuff -> build_rectangle
 def build_stuff():
+    # main_point -> rectangle_origin
     main_point = Point(50, 100)
+    # rect -> rectangle
     rect = Rectangle(main_point, 90, 10)
 
     return rect
 
 
+# my_rect -> rectangle
 my_rect = build_stuff()
 
 print(my_rect.area())
