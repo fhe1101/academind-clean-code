290bd9a8a2ca88f3ea0bfa8d59a476176bd930c1
Fusen He <efh003@gmail.com>
2024-02-02 08:08:16 -0500

    Add comments challenge analysis comments

--- challenge-problem.py
+++ challenge-problem.py
@@ -1,5 +1,8 @@
 # (c) Maximilian Schwarzmüller / Academind GmbH
 
+# <-- legal comments are all right
+
+# --> these marks and dividers are bad, should be removed.
 # *********
 # Imports
 # *********
@@ -9,8 +12,10 @@ from pathlib import Path
 # *********
 # Main
 # *********
-# A class which allows us to create DiskStorage instances
 
+# --> this is not a valid documentation. If needed, use python standard documentation style
+
+# A class which allows us to create DiskStorage instances
 
 class DiskStorage:
     def __init__(self, directory_name):
@@ -19,19 +24,25 @@ class DiskStorage:
     def get_directory_path(self):
         return Path(self.storage_directory)
 
+    # --> warnings can make sense. this is not what the code will fail.
     # This must be called before a file is inserted
     def create_directory(self):
         if (not path.exists(self.get_directory_path())):
             makedirs(self.storage_directory)
 
+    # --> still not the best place
     # Warning: Directory must exist in advance
     def insert_file(self, file_name, content):
+        # --> opportunity to reduce horizontal space for this line
         file = open(self.get_directory_path() / file_name, 'w')
         file.write(content)
         file.close()
+
+        # -> this is ok in a short term.
         # Todo: Add proper error handling
 
 
+# these two lines do not need line spacing
 log_storage = DiskStorage('logs')
 
 log_storage.insert_file('test.txt', 'Test')
